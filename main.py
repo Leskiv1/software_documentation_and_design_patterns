f9d1e5bb94c15adcad16cb5161bc5fb8ed7ecbdb
Leskiv1 <Nazareth56778@gmail.com>
2026-04-03 16:25:48 +0300

    Add lab_3

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
 from data_access.database import init_db
-from presentation.routers import router
+from presentation.routers import router as api_router
+from presentation.web_routers import router as web_router
 
 init_db()
 
 app = FastAPI(
-    title="Steam App API (Lab 2)",
+    title="Steam App API (Lab 3)",
     docs_url=None,
     redoc_url=None
 )
 
-app.include_router(router)
+app.include_router(api_router)
+
+app.include_router(web_router)
 
 @app.get("/")
 def root():
-    return {"message": "API працює!"}
+    return {"message": "API працює! Перейдіть на /games для перегляду каталогу."}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
